2a590da80a59ccbae2f4e343e7053bc13fdd5116
object-ions <moshikoatia@gmail.com>
2023-07-22 23:58:31 -0700

    add local storage option

--- app.js
+++ app.js
@@ -6,23 +6,41 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value; //the + turn it into data dypr number
 
-// Updated tottal in count
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', moviPrice);
+};
+
+// Updated tottal and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  //Copy selected seats into arr
+  //Map through arr
+  //Return a new array indexes:
+
+  let seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
 
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
-
-
-  console.log(selectedSeatsCount);
 }
 
 //Movie select event
 movieSelect.addEventListener('click', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
+
+//Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  console.log(selectedSeats);
+}
 
 //Movie select event
 movieSelect.addEventListener('change', (e) => {
